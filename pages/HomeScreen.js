f322744079b52905cd5d02c08f4f39e78c56f703
Najeebullah04 <najeebullahhaidari04@gmail.com>
2023-06-16 15:08:35 +0500

    Photos! is coppied

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -8,6 +8,7 @@ function Photos() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Photos!</Text>
+      <Text>Photos!</Text>
     </View>
   );
 }
